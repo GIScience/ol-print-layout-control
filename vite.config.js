e6aa096aa0ffb0607877b28f04b3f0f6b0f57b77
Michael Auer <michael.auer@heigit.org>
2022-10-01 00:21:03 +0200

    fix vite.config.js to run dev-server, build, test and coverage

--- vite.config.js
+++ vite.config.js
@@ -2,37 +2,63 @@ import {defineConfig} from 'vite'
 import {resolve} from "path";
 import typescript from "rollup-plugin-typescript2";
 
-export default defineConfig({
-    test: {
-        environment: 'happy-dom', // or 'jsdom', 'node',
-        dir: 'test',
-    }, root: 'vite-dev', build: {
-        outDir: '../dist', emptyOutDir: true, target: 'modules', //modules: support dynamic imports defined in ES2020
-        lib: {
-            entry: resolve(__dirname, 'src/ol-print-layout-control.ts'),
-            name: 'ol-print-layout-control', // fileName: 'PrintLayout',
-            fileName: () => 'ol-print-layout-control.js',
-            formats: ['umd']
-        }, sourcemap: true, rollupOptions: {
-            external: ['ol', 'ol/proj', 'ol/sphere', 'ol/size', 'ol/control/Control',], output: {
-                globals: {
-                    ol: 'ol',
-                    "ol/sphere": 'ol.sphere',
-                    "ol/proj": 'ol.proj',
-                    "ol/control/Control": 'ol.control.Control',
-                }, assetFileNames: (chunkInfo) => {
-                    if (chunkInfo.name === 'style.css') {
-                        return 'ol-print-layout-control.css';
-                    }
-                },
+export default defineConfig(({command, mode, ssrBuild}) => {
+    console.log(command, mode);
+    let baseConfig = {
+        test: {
+            environment: 'happy-dom', // or 'jsdom', 'node',
+            dir: 'test',
+            coverage:{
+                reportsDirectory: '../coverage'
+            }
+        },
+        root: 'vite-dev',
+        build: {
+            outDir: '../dist',
+            emptyOutDir: true,
+            target: 'modules', //modules: support dynamic imports defined in ES2020
+            lib: {
+                entry: resolve(__dirname, 'src/ol-print-layout-control.ts'),
+                name: 'ol-print-layout-control', // fileName: 'PrintLayout',
+                fileName: () => 'ol-print-layout-control.js',
+                formats: ['umd']
             },
-        }
-    }, plugins: [{
+            sourcemap: true,
+            rollupOptions: {
+                external: ['ol', 'ol/proj', 'ol/sphere', 'ol/size', 'ol/control/Control',],
+                output: {
+                    globals: {
+                        ol: 'ol',
+                        "ol/sphere": 'ol.sphere',
+                        "ol/proj": 'ol.proj',
+                        "ol/control/Control": 'ol.control.Control',
+                    }, assetFileNames: (chunkInfo) => {
+                        if (chunkInfo.name === 'style.css') {
+                            return 'ol-print-layout-control.css';
+                        }
+                    },
+                }
+            }
+        },
+        plugins:[]
+    };
+
+    const buildTypescriptDeclarationFilePlugin = {
         ...typescript({
             tsconfig: "tsconfig.json",
             clean: true,
             abortOnError: false,
             check: true
-        }), enforce: 'pre',
-    }]
+        }),
+        enforce: 'pre',
+    }
+
+    if ( command === 'build') {
+        baseConfig.plugins.push(buildTypescriptDeclarationFilePlugin)
+        return baseConfig;
+    } else{
+        //serve development | test
+       return baseConfig;
+    }
+
 })
