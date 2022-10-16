27227e95df79d15cb9637125ec9f8dd79f1e0e0a
Michael Auer <michael.auer@heigit.org>
2022-10-16 17:26:08 +0200

    add user input elements to dev page

--- vite-dev/main.ts
+++ vite-dev/main.ts
@@ -20,14 +20,105 @@ const map = new Map({
 const printLayoutControl = new PrintLayout({
     format: PAPER_FORMAT.A4,
     orientation: ORIENTATION.PORTRAIT,
-    margin: {top: 3, left: 2, bottom: 3, right: 2}
+    margin: {top: 3, bottom: 3, left: 2, right: 2}
 });
 map.addControl(printLayoutControl);
 
-//show current properties
-printLayoutControl.on('change', ()=>{
-    document.getElementById('currentProperties').innerText = `${JSON.stringify(printLayoutControl.getProperties(),null,2)}`
+// bind input elements
+function fillSelectOptions(selectElementId, optionsMap) {
+    const selectElement = document.getElementById(selectElementId);
+    for (const paperformatKey in optionsMap) {
+        const option = document.createElement('option');
+        option.text = optionsMap[paperformatKey];
+        selectElement.appendChild(option);
+    }
+}
+
+function setFormValue(id: string, value: string) {
+    const formInput = <HTMLInputElement>document.getElementById(id);
+    formInput.value = value;
+}
+
+interface FormatHTMLSelectElement extends HTMLSelectElement {
+    value: typeof PAPER_FORMAT[keyof typeof PAPER_FORMAT]
+}
+
+interface OrientationHTMLSelectElement extends HTMLSelectElement {
+    value: ORIENTATION
+}
+
+function bindFormEvent(id: string, eventType: keyof HTMLElementEventMap, fn: EventListenerOrEventListenerObject) {
+    const formInput = <HTMLInputElement>document.getElementById(id);
+    formInput.addEventListener(eventType, fn);
+}
+
+//fill form options, set initial form value from ol-Control and bind listeners to update View and Mode in both ways
+
+// property: format
+fillSelectOptions('format', PAPER_FORMAT);
+setFormValue('format', printLayoutControl.getFormat());
+bindFormEvent('format', 'change', (event) => {
+    const target = <FormatHTMLSelectElement>event.target;
+    printLayoutControl.setFormat(target.value);
+});
+// @ts-ignore
+printLayoutControl.on('change:format', (event) => {
+    setFormValue('format', event.target.getFormat());
+});
+
+// property: orientation
+fillSelectOptions('orientation', ORIENTATION);
+setFormValue('orientation', printLayoutControl.getOrientation());
+bindFormEvent('orientation', 'change', (event) => {
+    const target = <OrientationHTMLSelectElement>event.target;
+    printLayoutControl.setOrientation(target.value);
 })
+// @ts-ignore
+printLayoutControl.on('change:orientation', (event) => {
+    setFormValue('orientation', printLayoutControl.getOrientation());
+});
+
+// property: margin
+setFormValue('marginTop', printLayoutControl.getMargin().getTop());
+bindFormEvent('marginTop', 'change', (event) => {
+    const target = <HTMLInputElement>event.target;
+    printLayoutControl.getMargin().setTop(Number(target.value));
+});
+
+setFormValue('marginBottom', printLayoutControl.getMargin().getBottom());
+bindFormEvent('marginBottom', 'change', (event) => {
+    const target = <HTMLInputElement>event.target;
+    printLayoutControl.getMargin().setBottom(Number(target.value));
+});
+
+setFormValue('marginLeft', printLayoutControl.getMargin().getLeft());
+bindFormEvent('marginLeft', 'change', (event) => {
+    const target = <HTMLInputElement>event.target;
+    printLayoutControl.getMargin().setLeft(Number(target.value));
+});
+
+setFormValue('marginRight', printLayoutControl.getMargin().getRight());
+bindFormEvent('marginRight', 'change', (event) => {
+    const target = <HTMLInputElement>event.target;
+    printLayoutControl.getMargin().setRight(Number(target.value));
+});
+
+
+// @ts-ignore
+printLayoutControl.on('change:margin', (event) => {
+    const prtLytCtrl = event.target;
+    setFormValue('marginTop', prtLytCtrl.getMargin().getTop());
+    setFormValue('marginBottom', prtLytCtrl.getMargin().getBottom());
+    setFormValue('marginLeft', prtLytCtrl.getMargin().getLeft());
+    setFormValue('marginRight', prtLytCtrl.getMargin().getRight());
+})
+
+
+//show current properties
+printLayoutControl.on('change', () => {
+    document.getElementById('marginProperties').innerText = `${JSON.stringify(printLayoutControl.getProperties(), null, 2)}`
+})
+
 
 // make it globally available for debugging
 // @ts-ignore
